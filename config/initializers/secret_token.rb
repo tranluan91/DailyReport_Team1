f13d2ec4853161b8e2032aa1e8c1c547be29a35c
hungcoj <bach.dang.hung@framgia.com>
2013-08-03 09:30:04 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyReportTeam1::Application.config.secret_token = 'fe45025dd1bc7e0f2ae9c159a1c4ecaaae1ff8c23b6ef06fc661ad1032eea2eb2bea9f7035f0b3e0b604ce4ca2e3ce9743ef1925d7660f617a967cd101d1f650'
